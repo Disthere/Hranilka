32840f06d4e59bba0ad291e42786efc3f30fba11
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-05-28 17:59:36 +0500

    Базовый класс модели-представления

---
+++ ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hranilka.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        // INotifyPropertyChanged -  Когда объект класса изменяет значение свойства, то он через событие PropertyChanged
+        // извещает систему об изменении свойства. А система обновляет все привязанные объекты.
+        // Автообновление визуальной части текстовых блоков 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field,value))
+            {
+                return false;
+            }
+
+            field = value;
+
+            OnPropertyChanged(PropertyName);
+
+            return true;
+        }
+    }
+}
