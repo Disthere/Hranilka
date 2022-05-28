6666ee0f19f2b6b0a4110f5cfc604f571f3d51d0
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-05-28 23:19:56 +0500

    Разметка (DocPanel) Вкладки

--- ViewModels/Base/ViewModelBase.cs
+++ ViewModels/Base/ViewModelBase.cs
@@ -33,5 +33,7 @@ namespace Hranilka.ViewModels.Base
 
             return true;
         }
+
+        
     }
 }
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Hranilka.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hranilka.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+        private string title = "Hranilka V1.0";
+
+       /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+            //set
+            //{
+            //    if (Equals(title, value))
+            //    {
+            //        return;
+            //    }
+
+            //    title = value;
+
+            //    OnPropertyChanged();
+
+            //}
+        }
+        #endregion
+    }
+}
